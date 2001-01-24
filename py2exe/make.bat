db0365dda0d2122110644721a9cdf13db0a651fb
Thomas Heller <theller@ctypes.org>
2001-01-24 13:07:10 +0000

    *** empty log message ***

--- py2exe/make.bat
+++ py2exe/make.bat
@@ -1,9 +1,12 @@
-rd /s/q build dist
+setlocal
+call vcvars32
+rd /s/q build
 call py20 setup.py build
 call py20 setup.py build -g
 call py20 setup.py bdist_wininst --dist-dir upload --target-version 2.0
+del upload\py2exe*.zip
 call py20 setup.py sdist --dist-dir upload
-rd /s/q build dist
+rd /s/q build
 call py15 setup.py build
 call py15 setup.py build -g
 call py15 setup.py bdist_wininst --dist-dir upload --target-version 1.5
