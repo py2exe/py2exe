ee32376abbf747e43ae37f7e801243fcd0e46050
Thomas Heller <theller@ctypes.org>
2001-01-30 21:11:20 +0000

    Updated several files. Changes to setup.py: Cannot use clib, because a bug prevents building a source distribution.

--- py2exe/make.bat
+++ py2exe/make.bat
@@ -5,6 +5,8 @@ call py20 setup.py build
 call py20 setup.py build -g
 call py20 setup.py bdist_wininst --dist-dir upload --target-version 2.0
 del upload\py2exe*.zip
+call py20 setup.py sdist -o
+call py20 setup.py sdist -o
 call py20 setup.py sdist --dist-dir upload
 rd /s/q build
 call py15 setup.py build
