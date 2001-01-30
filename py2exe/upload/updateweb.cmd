5f40e4108862cccbe6f1db9a6ef6508792940eeb
Thomas Heller <theller@ctypes.org>
2001-01-30 21:19:23 +0000

    Preparing for the 0.2 release tomorrow.

--- py2exe/upload/updateweb.cmd
+++ py2exe/upload/updateweb.cmd
@@ -1,1 +1,1 @@
-scp py2exe-0.1.1.win32-py1.5.exe py2exe-0.1.1.win32-py2.0.exe py2exe-0.1.1.zip theller@starship.python.net:~/public_html/py2exe
+scp py2exe-0.2.0.win32-py1.5.exe py2exe-0.2.0.win32-py2.0.exe py2exe-0.2.0.zip theller@starship.python.net:~/public_html/py2exe
