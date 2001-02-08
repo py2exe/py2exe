501390b67115e1665e7c81bbb298c935846ddaa7
theller <theller@b94a6edd-cd4f-0410-beef-d2780d74e76e>
2001-02-08 11:07:58 +0000

    At least WE should use PyImport_Import instead of PyImport_ImportModule!

--- trunk/py2exe/source/start.c
+++ trunk/py2exe/source/start.c
@@ -244,7 +244,7 @@ BOOL Load_Module(char *name)
     if (data) {
 	PyObject *marshaldict;
 	PyObject *module;
-	PyObject *marshal = PyImport_ImportModule("marshal");
+	PyObject *marshal = PyImport_Import("marshal");
 	PyObject *loadsfunc;
 	PyObject *pdata = PyString_FromStringAndSize(data+8, size-8);
 
