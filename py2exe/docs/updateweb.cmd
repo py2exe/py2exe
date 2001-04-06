879e2cb9a37c439f973e85940e3f83e0f1370967
Thomas Heller <theller@ctypes.org>
2001-04-06 13:59:23 +0000

    V 0.2.5.

--- py2exe/docs/updateweb.cmd
+++ py2exe/docs/updateweb.cmd
@@ -1,2 +1,2 @@
-scp py2exe.jpg index.html py2exe.css theller@starship.python.net:~/public_html/py2exe
-scp py2exe.jpg index.html py2exe.css theller@shell.sourceforge.net:/home/groups/py2exe/htdocs
+scp LICENSE.txt py2exe.jpg index.html py2exe.css theller@starship.python.net:~/public_html/py2exe
+scp LICENSE.txt py2exe.jpg index.html py2exe.css theller@shell.sourceforge.net:/home/groups/py2exe/htdocs
