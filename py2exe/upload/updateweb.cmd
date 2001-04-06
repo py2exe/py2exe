3ef0d454d2d15d25f9656de2dfe23ff870f63b0c
Thomas Heller <theller@ctypes.org>
2001-04-06 13:13:34 +0000

    Version 0.2.5.

--- py2exe/upload/updateweb.cmd
+++ py2exe/upload/updateweb.cmd
@@ -1,1 +1,1 @@
-scp py2exe-0.2.2.win32-py1.5.exe py2exe-0.2.2.win32-py2.0.exe py2exe-0.2.2.zip theller@starship.python.net:~/public_html/py2exe
+scp py2exe-0.2.5.win32-py1.5.exe py2exe-0.2.5.win32-py2.0.exe py2exe-0.2.5.zip ..\LICENSE.txt theller@starship.python.net:~/public_html/py2exe
