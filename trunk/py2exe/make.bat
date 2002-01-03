a71ce128ff0aa1b2bce1d1ff15ba286c65694daa
theller <theller@b94a6edd-cd4f-0410-beef-d2780d74e76e>
2002-01-03 13:28:46 +0000

    Debug versions no longer included.

--- trunk/py2exe/make.bat
+++ trunk/py2exe/make.bat
@@ -2,7 +2,7 @@ setlocal
 call vcvars32
 rd /s/q build
 call py20 setup.py build
-call py20 setup.py build -g
+REM call py20 setup.py build -g
 call py20 setup.py bdist_wininst --dist-dir upload --target-version 2.0
 del upload\py2exe*.zip
 call py20 setup.py sdist -o
@@ -10,9 +10,13 @@ call py20 setup.py sdist -o
 call py20 setup.py sdist --dist-dir upload
 rd /s/q build
 call py15 setup.py build
-call py15 setup.py build -g
+REM call py15 setup.py build -g
 call py15 setup.py bdist_wininst --dist-dir upload --target-version 1.5
 rd /s/q build
 call py21 setup.py build
-call py21 setup.py build -g
+REM call py21 setup.py build -g
 call py21 setup.py bdist_wininst --dist-dir upload --target-version 2.1
+rd /s/q build
+call py22 setup.py build
+REM call py22 setup.py build -g
+call py22 setup.py bdist_wininst --dist-dir upload --target-version 2.2
