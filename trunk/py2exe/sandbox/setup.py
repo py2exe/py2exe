44bd3ae3cff7c54617cc2a7323b9b58fbf2e10af
mhammond <mhammond@b94a6edd-cd4f-0410-beef-d2780d74e76e>
2003-08-29 12:40:38 +0000

    Re-enable service support, but only if we can find the pywin32 build directory (whereas previously we asked the user to disable it!)

--- trunk/py2exe/sandbox/setup.py
+++ trunk/py2exe/sandbox/setup.py
@@ -296,79 +296,78 @@ run_dll = Interpreter("py2exe.run_dll",
                     target_desc = "shared_library",
                     )
 
-##msg = """PYWIN32DIR invalid.
 
-##    To build py2exe from source, you must either:
+msg = """PYWIN32DIR invalid.
 
-##    - Download and build Mark Hammond's pywin32 source code
-##      from http://starship.python.net/crew/mhammond, and set
-##      the PYWIN32DIR variable to point to this directory
-
-##    - or change this setup script to not build run_svc target,
-##      then you will not be able to build Windows NT services
-##      with py2exe.
-##"""
-##def _check_build_dir(map, key, source_root_name, child):
-##    check = os.path.join(source_root_name, child)
-##    if os.path.isdir(check):
-##        map[key] = check
-##        return True
-##    return False
+    To build py2exe from source and be able to build services, you must
+    download and build Mark Hammond's pywin32 source code
+    from http://starship.python.net/crew/mhammond, and set
+    the PYWIN32DIR variable to point to this directory
+    
+    py2exe will be configured without Windows Service support
+"""
+def _check_build_dir(map, key, source_root_name, child):
+    check = os.path.join(source_root_name, child)
+    if os.path.isdir(check):
+        map[key] = check
+        return True
+    return False
         
-##def _get_build_dirs(source_root_name):
-##    ret = {}
-##    if _check_build_dir(ret, "win32", source_root_name, "build/temp.win32-%s" % sys.winver):
-##        pass
-##    elif _check_build_dir(ret, "win32", source_root_name, "win32/build"):
-##        pass
-##    # todo: add com etc maybe?
-##    return ret
+def _get_build_dirs(source_root_name):
+    ret = {}
+    if _check_build_dir(ret, "win32", source_root_name, "build/temp.win32-%s" % sys.winver):
+        pass
+    elif _check_build_dir(ret, "win32", source_root_name, "win32/build"):
+        pass
+    # todo: add com etc maybe?
+    return ret
 
-##PYWIN32DIR = os.environ.get("PYWIN32DIR", os.path.abspath(r"\pywin32"))
-##build_dirs = _get_build_dirs(PYWIN32DIR)
-##if not build_dirs.has_key("win32"):
-##    # Check dev studio build running in place
-##    # win32api.__file__: 'prefix\\pywin32\\win32\\Build\\win32api.pyd'
-##    # PYWIN32DIR -> 'prefix\\pywin32'
-##    try:
-##        import win32api
-##    except ImportError:
-##        raise RuntimeError, msg
+PYWIN32DIR = os.environ.get("PYWIN32DIR", os.path.abspath(r"\pywin32"))
+build_dirs = _get_build_dirs(PYWIN32DIR)
+if not build_dirs.has_key("win32"):
+    # Check dev studio build running in place
+    # win32api.__file__: 'prefix\\pywin32\\win32\\Build\\win32api.pyd'
+    # PYWIN32DIR -> 'prefix\\pywin32'
+    try:
+        import win32api
+    except ImportError:
+        raise RuntimeError, msg
 
-##    PYWIN32DIR = os.path.dirname(os.path.dirname(os.path.dirname(win32api.__file__)))
-##    build_dirs = _get_build_dirs(PYWIN32DIR)
+    PYWIN32DIR = os.path.dirname(os.path.dirname(os.path.dirname(win32api.__file__)))
+    build_dirs = _get_build_dirs(PYWIN32DIR)
 
-##if not build_dirs.has_key("win32"):
-##    raise RuntimeError, msg
-
-##pywintypes_lib = os.path.join(build_dirs["win32"], "pywintypes.lib")
-##if not os.path.isfile(pywintypes_lib):
-##    raise RuntimeError, msg
-
-##run_svc = Interpreter("py2exe.run_svc",
-##                      ["source/PythonService.cpp",
-##                       "source/run_svc.c", "source/start.c",
-##                       PYWIN32DIR + "/win32/src/PythonServiceMessages.mc"
-##                       ],
-##                      include_dirs=["source/zlib",
-##                                    PYWIN32DIR + "/win32/src"],
-##                      libraries=["zlibstat", "user32", "advapi32",
-##                                 "oleaut32", "ole32", "shell32"],
-##                      library_dirs=["source/zlib/static32",
-##                                    build_dirs["win32"]],
-##                      extra_link_args=["/NOD:LIBC"],
-##                      define_macros=[("ZLIB_DLL", None),
-##                                     ("_WINDOWS", None),
-##                                     ("BUILD_FREEZE", None),
-##                                     ("BUILD_PY2EXE", None),
-##                                     ("WIN32", None),
-##                                     ("__WIN32__", None),
-####                                     ("_UNICODE", None),
-####                                     ("UNICODE", None),
-##                                     ("NDEBUG", None),
-##                                     ("STRICT", None)
-##                                     ]
-##                      )
+if not build_dirs.has_key("win32"):
+    print "*" * 75
+    print msg
+    print "py2exe will be unable to build a service"
+    print "*" * 75
+    run_svc = None
+else:
+    pywintypes_lib = os.path.join(build_dirs["win32"], "pywintypes.lib")
+    if not os.path.isfile(pywintypes_lib):
+        raise RuntimeError, msg
+    
+    run_svc = Interpreter("py2exe.run_svc",
+                          ["source/PythonService.cpp",
+                           "source/run_svc.c", "source/start.c",
+                           PYWIN32DIR + "/win32/src/PythonServiceMessages.mc"
+                           ],
+                          include_dirs=["source/zlib",
+                                        PYWIN32DIR + "/win32/src"],
+                          libraries=["user32", "advapi32",
+                                     "oleaut32", "ole32", "shell32"],
+                          library_dirs=["source/zlib/static32",
+                                        build_dirs["win32"]],
+                          define_macros=[("ZLIB_DLL", None),
+                                         ("_WINDOWS", None),
+                                         ("BUILD_FREEZE", None),
+                                         ("BUILD_PY2EXE", None),
+                                         ("WIN32", None),
+                                         ("__WIN32__", None),
+                                         ("NDEBUG", None),
+                                         ("STRICT", None)
+                                         ]
+                          )
 
 ##run_dll = Interpreter("py2exe.run_dll",
 ##                    ["source/run_dll.c", "source/start.c", "source/icon.rc"],
@@ -380,6 +379,9 @@ run_dll = Interpreter("py2exe.run_dll",
 ##                    target_desc = "shared_library",
 ##                    )
 
+interpreters = [run, run_w, run_dll]
+if run_svc:
+    interpreters.append(run_svc)
 
 setup(name="py2exe",
       version=__version__,
@@ -400,10 +402,8 @@ setup(name="py2exe",
                                sources=["source/py2exe_util.c"],
                                libraries=["imagehlp"]),
                     ],
-##      interpreters = [run, run_w, run_svc, run_dll],
-      interpreters = [run, run_w, run_dll],
-##      packages=['py2exe', 'py2exe.resources', 'py2exe.tools'],
-      packages=['py2exe'],
+      interpreters = interpreters,
+      packages=['py2exe', 'py2exe.resources'],
       )
 
 # Local Variables:
