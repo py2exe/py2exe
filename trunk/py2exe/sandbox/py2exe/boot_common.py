6e6be2d9c35be5f0dca97ebde4b6cc934bfb3889
mhammond <mhammond@b94a6edd-cd4f-0410-beef-d2780d74e76e>
2003-09-16 08:50:52 +0000

    Take advantage of multiple code objects, and have a "boot_common" script. This prevents us duplicating the linecache code everywhere, and makes it available to standard .exe code.

---
+++ trunk/py2exe/sandbox/py2exe/boot_common.py
@@ -0,0 +1,15 @@
+# Common py2exe boot script - executed for all target types.
+
+# Disable linecache.getline() which is called by
+# traceback.extract_stack() when an exception occurs to try and read
+# the filenames embedded in the packaged python code.  This is really
+# annoying on windows when the d: or e: on our build box refers to
+# someone elses removable or network drive so the getline() call
+# causes it to ask them to insert a disk in that drive.
+import linecache
+def fake_getline(filename, lineno):
+    return ''
+linecache.orig_getline = linecache.getline
+linecache.getline = fake_getline
+
+print "common Boot script"
