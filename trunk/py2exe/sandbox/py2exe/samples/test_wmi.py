bad2c32e9b9e8345e068744f70dd28addf2456c0
theller <theller@b94a6edd-cd4f-0410-beef-d2780d74e76e>
2003-10-28 09:02:02 +0000

    Work around a small problem in win32all build 161.

--- trunk/py2exe/sandbox/py2exe/samples/test_wmi.py
+++ trunk/py2exe/sandbox/py2exe/samples/test_wmi.py
@@ -1,5 +1,8 @@
 import wmi # Tim Golden's wmi module.
 
+# Workaround for a bug in win32all 161, see Mark Hammond's download page:
+import win32com.client.util
+
 computer = wmi.WMI()
 
 for item in computer.Win32_Process()[:2]:
