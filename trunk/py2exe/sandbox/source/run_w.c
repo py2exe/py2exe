afe71c7513d3bf8e3391292af56e099d05eb85ae
theller <theller@b94a6edd-cd4f-0410-beef-d2780d74e76e>
2003-11-04 08:55:19 +0000

    Whitespace cleanup.

--- trunk/py2exe/sandbox/source/run_w.c
+++ trunk/py2exe/sandbox/source/run_w.c
@@ -26,28 +26,28 @@
 
 void SystemError(int error, char *msg)
 {
-    char Buffer[1024];
-    int n;
-    
-    if (error) {
-        LPVOID lpMsgBuf;
-	FormatMessage( 
-	    FORMAT_MESSAGE_ALLOCATE_BUFFER | 
-	    FORMAT_MESSAGE_FROM_SYSTEM,
-	    NULL,
-	    error,
-	    MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-	    (LPSTR)&lpMsgBuf,
-	    0,
-	    NULL 
-	    );
-        strncpy(Buffer, lpMsgBuf, sizeof(Buffer));
-	LocalFree(lpMsgBuf);
-    } else
-	Buffer[0] = '\0';
-    n = lstrlen(Buffer);
-    _snprintf(Buffer+n, sizeof(Buffer)-n, msg);
-    MessageBox(GetFocus(), Buffer, NULL, MB_OK | MB_ICONSTOP);
+	char Buffer[1024];
+	int n;
+
+	if (error) {
+		LPVOID lpMsgBuf;
+		FormatMessage( 
+			FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+			FORMAT_MESSAGE_FROM_SYSTEM,
+			NULL,
+			error,
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+			(LPSTR)&lpMsgBuf,
+			0,
+			NULL 
+			);
+		strncpy(Buffer, lpMsgBuf, sizeof(Buffer));
+		LocalFree(lpMsgBuf);
+	} else
+		Buffer[0] = '\0';
+	n = lstrlen(Buffer);
+	_snprintf(Buffer+n, sizeof(Buffer)-n, msg);
+	MessageBox(GetFocus(), Buffer, NULL, MB_OK | MB_ICONSTOP);
 }
 
 extern int init();
@@ -57,9 +57,9 @@ int WINAPI
 WinMain(HINSTANCE hInst, HINSTANCE hPrevInst,
 	LPSTR lpCmdLine, int nCmdShow)
 {
-    int result;
-    result = init();
-    if (result)
-	return result;
-    return start(__argc, __argv);
+	int result;
+	result = init();
+	if (result)
+		return result;
+	return start(__argc, __argv);
 }
