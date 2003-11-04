27c7d7ff1e79ef07baf2e893def15a91d95b1d16
Thomas Heller <theller@ctypes.org>
2003-11-04 08:55:19 +0000

    Whitespace cleanup.

--- py2exe/sandbox/source/run.c
+++ py2exe/sandbox/source/run.c
@@ -26,26 +26,26 @@
 
 void SystemError(int error, char *msg)
 {
-    char Buffer[1024];
-    
-    if (msg)
-	    fprintf(stderr, msg);
-    if (error) {
-        LPVOID lpMsgBuf;
-	FormatMessage( 
-	    FORMAT_MESSAGE_ALLOCATE_BUFFER | 
-	    FORMAT_MESSAGE_FROM_SYSTEM,
-	    NULL,
-	    error,
-	    MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-	    (LPSTR)&lpMsgBuf,
-	    0,
-	    NULL 
-	    );
-        strncpy(Buffer, lpMsgBuf, sizeof(Buffer));
-	LocalFree(lpMsgBuf);
-	fprintf(stderr, Buffer);
-    }
+	char Buffer[1024];
+
+	if (msg)
+		fprintf(stderr, msg);
+	if (error) {
+		LPVOID lpMsgBuf;
+		FormatMessage( 
+			FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+			FORMAT_MESSAGE_FROM_SYSTEM,
+			NULL,
+			error,
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+			(LPSTR)&lpMsgBuf,
+			0,
+			NULL 
+			);
+		strncpy(Buffer, lpMsgBuf, sizeof(Buffer));
+		LocalFree(lpMsgBuf);
+		fprintf(stderr, Buffer);
+	}
 }
 
 extern int init(void);
@@ -53,9 +53,9 @@ extern int start(int argc, char **argv);
 
 int main (int argc, char **argv)
 {
-    int result;
-    result = init();
-    if (result)
-	return result;
-    return start(argc, argv);
+	int result;
+	result = init();
+	if (result)
+		return result;
+	return start(argc, argv);
 }
--- py2exe/sandbox/source/run_dll.c
+++ py2exe/sandbox/source/run_dll.c
@@ -38,28 +38,28 @@ __PROC__PyCom_CoUninitialize PyCom_CoUninitialize = NULL;
 
 void SystemError(int error, char *msg)
 {
-    char Buffer[1024];
-    int n;
-    
-    if (error) {
-        LPVOID lpMsgBuf;
-    FormatMessage( 
-        FORMAT_MESSAGE_ALLOCATE_BUFFER | 
-        FORMAT_MESSAGE_FROM_SYSTEM,
-        NULL,
-        error,
-        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-        (LPSTR)&lpMsgBuf,
-        0,
-        NULL 
-        );
-        strncpy(Buffer, lpMsgBuf, sizeof(Buffer));
-    LocalFree(lpMsgBuf);
-    } else
-    Buffer[0] = '\0';
-    n = lstrlen(Buffer);
-    _snprintf(Buffer+n, sizeof(Buffer)-n, msg);
-    MessageBox(GetFocus(), Buffer, NULL, MB_OK | MB_ICONSTOP);
+	char Buffer[1024];
+	int n;
+
+	if (error) {
+		LPVOID lpMsgBuf;
+		FormatMessage( 
+			FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+			FORMAT_MESSAGE_FROM_SYSTEM,
+			NULL,
+			error,
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+			(LPSTR)&lpMsgBuf,
+			0,
+			NULL 
+			);
+		strncpy(Buffer, lpMsgBuf, sizeof(Buffer));
+		LocalFree(lpMsgBuf);
+	} else
+		Buffer[0] = '\0';
+	n = lstrlen(Buffer);
+	_snprintf(Buffer+n, sizeof(Buffer)-n, msg);
+	MessageBox(GetFocus(), Buffer, NULL, MB_OK | MB_ICONSTOP);
 }
 
 BOOL have_init = FALSE;
@@ -72,78 +72,78 @@ extern int run_script(void);
 
 int load_pythoncom(void)
 {
-    char dll_path[_MAX_PATH+_MAX_FNAME+1];
-    char *winver = "";
-    char major = 'x', minor = 'y';
+	char dll_path[_MAX_PATH+_MAX_FNAME+1];
+	char *winver = "";
+	char major = 'x', minor = 'y';
 #ifdef _DEBUG
-    char *suffix = "_d";
+	char *suffix = "_d";
 #else
-    char *suffix = "";
+	char *suffix = "";
 #endif
 
-    PyObject *ob_winver = PySys_GetObject("winver");
-    if (PyString_Check(ob_winver))
-        winver = PyString_AsString(ob_winver);
-    // works until Python 10!
-    if (winver && strlen(winver)>=3) {
-        major = winver[0];
-        minor = winver[2];
-    }
+	PyObject *ob_winver = PySys_GetObject("winver");
+	if (PyString_Check(ob_winver))
+		winver = PyString_AsString(ob_winver);
+	// works until Python 10!
+	if (winver && strlen(winver)>=3) {
+		major = winver[0];
+		minor = winver[2];
+	}
 
-    // shouldn't do this twice
-    assert(gPythoncom == NULL);
+	// shouldn't do this twice
+	assert(gPythoncom == NULL);
 
-    snprintf(dll_path, sizeof(dll_path), "pythoncom%c%c%s.dll", major, minor, suffix);
-    gPythoncom = GetModuleHandle(dll_path);
-    if (gPythoncom == NULL) {
-        // not already loaded - try and load from the current dir
-        char *temp;
-        GetModuleFileNameA(gInstance, dll_path, sizeof(dll_path));
-        temp = dll_path + strlen(dll_path);
-        while (temp>dll_path && *temp != '\\')
-            temp--;
-        // and printf directly in the buffer.
-        snprintf(temp, sizeof(dll_path)-strlen(temp),
-                 "pythoncom%c%c%s.dll", major, minor, suffix);
-        gPythoncom = LoadLibraryEx( dll_path, // points to name of executable module 
-                       NULL, // HANDLE hFile, // reserved, must be NULL 
-                       LOAD_WITH_ALTERED_SEARCH_PATH // DWORD dwFlags // entry-point execution flag 
-                      ); 
-    }
-    Py_XDECREF(ob_winver);
-    if (gPythoncom == NULL)
-        // give up in disgust
-        return -1;
+	snprintf(dll_path, sizeof(dll_path), "pythoncom%c%c%s.dll", major, minor, suffix);
+	gPythoncom = GetModuleHandle(dll_path);
+	if (gPythoncom == NULL) {
+		// not already loaded - try and load from the current dir
+		char *temp;
+		GetModuleFileNameA(gInstance, dll_path, sizeof(dll_path));
+		temp = dll_path + strlen(dll_path);
+		while (temp>dll_path && *temp != '\\')
+			temp--;
+		// and printf directly in the buffer.
+		snprintf(temp, sizeof(dll_path)-strlen(temp),
+			 "pythoncom%c%c%s.dll", major, minor, suffix);
+		gPythoncom = LoadLibraryEx(dll_path, // points to name of executable module 
+					   NULL, // HANDLE hFile, // reserved, must be NULL 
+					   LOAD_WITH_ALTERED_SEARCH_PATH // DWORD dwFlags // entry-point execution flag 
+			); 
+	}
+	Py_XDECREF(ob_winver);
+	if (gPythoncom == NULL)
+		// give up in disgust
+		return -1;
 
-    Pyc_DllCanUnloadNow = (__PROC__DllCanUnloadNow)GetProcAddress(gPythoncom, "DllCanUnloadNow");
-    Pyc_DllGetClassObject = (__PROC__DllGetClassObject)GetProcAddress(gPythoncom, "DllGetClassObject");
-    PyCom_CoUninitialize = (__PROC__PyCom_CoUninitialize)GetProcAddress(gPythoncom, "PyCom_CoUninitialize");
-    return 0;
+	Pyc_DllCanUnloadNow = (__PROC__DllCanUnloadNow)GetProcAddress(gPythoncom, "DllCanUnloadNow");
+	Pyc_DllGetClassObject = (__PROC__DllGetClassObject)GetProcAddress(gPythoncom, "DllGetClassObject");
+	PyCom_CoUninitialize = (__PROC__PyCom_CoUninitialize)GetProcAddress(gPythoncom, "PyCom_CoUninitialize");
+	return 0;
 }
 
 int check_init()
 {
-    if (!have_init) {
-        PyObject *frozen;
-        init_with_instance(gInstance);
-        // a little DLL magic.  Set sys.frozen='dll'
-        frozen = PyString_FromString("dll");
-        if (frozen) {
-            PySys_SetObject("frozen", frozen);
-            Py_DECREF(frozen);
-        }
-        frozen = PyInt_FromLong((LONG)gInstance);
-        if (frozen) {
-            PySys_SetObject("frozendllhandle", frozen);
-            Py_DECREF(frozen);
-        }
-        // Now run the generic script - this always returns in a DLL.
-        run_script();
-        have_init = TRUE;
-    }
-    if (gPythoncom == NULL)
-        load_pythoncom();
-    return gPythoncom != NULL;
+	if (!have_init) {
+		PyObject *frozen;
+		init_with_instance(gInstance);
+		// a little DLL magic.  Set sys.frozen='dll'
+		frozen = PyString_FromString("dll");
+		if (frozen) {
+			PySys_SetObject("frozen", frozen);
+			Py_DECREF(frozen);
+		}
+		frozen = PyInt_FromLong((LONG)gInstance);
+		if (frozen) {
+			PySys_SetObject("frozendllhandle", frozen);
+			Py_DECREF(frozen);
+		}
+		// Now run the generic script - this always returns in a DLL.
+		run_script();
+		have_init = TRUE;
+	}
+	if (gPythoncom == NULL)
+		load_pythoncom();
+	return gPythoncom != NULL;
 }
 
 
@@ -153,56 +153,56 @@ int check_init()
 // *****************************************************************
 BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
 {
-    if ( dwReason == DLL_PROCESS_ATTACH) {
-        gInstance = hInstance;
-    }
-    else if ( dwReason == DLL_PROCESS_DETACH ) {
-        gInstance = 0;
-        // not much else safe to do here
-    }
-    return TRUE; 
+	if ( dwReason == DLL_PROCESS_ATTACH) {
+		gInstance = hInstance;
+	}
+	else if ( dwReason == DLL_PROCESS_DETACH ) {
+		gInstance = 0;
+		// not much else safe to do here
+	}
+	return TRUE; 
 }
 
 HRESULT __stdcall DllCanUnloadNow(void)
 {
-    HRESULT rc;
-    check_init();
-    assert(Pyc_DllCanUnloadNow);
-    if (!Pyc_DllCanUnloadNow) return E_UNEXPECTED;
-    rc = Pyc_DllCanUnloadNow();
-    //if (rc == S_OK && PyCom_CoUninitialize)
-    //  PyCom_CoUninitialize();
-    return rc;
+	HRESULT rc;
+	check_init();
+	assert(Pyc_DllCanUnloadNow);
+	if (!Pyc_DllCanUnloadNow) return E_UNEXPECTED;
+	rc = Pyc_DllCanUnloadNow();
+	//if (rc == S_OK && PyCom_CoUninitialize)
+	//  PyCom_CoUninitialize();
+	return rc;
 }
 
 //__declspec(dllexport) int __stdcall DllGetClassObject(void *rclsid, void *riid, void *ppv)
 HRESULT __stdcall DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID *ppv)
 {
-    HRESULT rc;
-    check_init();
-    assert(Pyc_DllGetClassObject);
-    if (!Pyc_DllGetClassObject) return E_UNEXPECTED;
-    rc = Pyc_DllGetClassObject(rclsid, riid, ppv);
-    return rc;
+	HRESULT rc;
+	check_init();
+	assert(Pyc_DllGetClassObject);
+	if (!Pyc_DllGetClassObject) return E_UNEXPECTED;
+	rc = Pyc_DllGetClassObject(rclsid, riid, ppv);
+	return rc;
 }
 
 
 STDAPI DllRegisterServer()
 {
-    int rc=0;
-    check_init();
-    rc = PyRun_SimpleString("DllRegisterServer()\n");
-    if (rc != 0)
-        PyErr_Print();
-    return rc==0 ? 0 : SELFREG_E_CLASS;
+	int rc=0;
+	check_init();
+	rc = PyRun_SimpleString("DllRegisterServer()\n");
+	if (rc != 0)
+		PyErr_Print();
+	return rc==0 ? 0 : SELFREG_E_CLASS;
 }
 
 STDAPI DllUnregisterServer()
 {
-    int rc=0;
-    check_init();
-    rc = PyRun_SimpleString("DllUnregisterServer()\n");
-    if (rc != 0)
-        PyErr_Print();
-    return rc==0 ? 0 : SELFREG_E_CLASS;
+	int rc=0;
+	check_init();
+	rc = PyRun_SimpleString("DllUnregisterServer()\n");
+	if (rc != 0)
+		PyErr_Print();
+	return rc==0 ? 0 : SELFREG_E_CLASS;
 }
