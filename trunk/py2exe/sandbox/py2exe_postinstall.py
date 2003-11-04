95e40c93278a27980fbdf26a65e30d78856e4d77
theller <theller@b94a6edd-cd4f-0410-beef-d2780d74e76e>
2003-11-04 08:09:55 +0000

    Added a bdist_wininst post-install script. Currently it only displays a short message in the last screen when the installer is run.

---
+++ trunk/py2exe/sandbox/py2exe_postinstall.py
@@ -0,0 +1,8 @@
+"""py2exe is now installed on your machine.
+
+There are some samples in the 'samples' subdirectory."""
+
+import sys
+
+if len(sys.argv) == 2 and sys.argv[1] == "-install":
+    print __doc__
