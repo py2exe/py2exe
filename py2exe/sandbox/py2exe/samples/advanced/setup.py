9b01898b634845b1b93fc395297d643f56908e30
Thomas Heller <theller@ctypes.org>
2003-12-30 14:44:32 +0000

    Trivial fixes to make the samples more useful.

--- py2exe/sandbox/py2exe/samples/advanced/setup.py
+++ py2exe/sandbox/py2exe/samples/advanced/setup.py
@@ -2,6 +2,7 @@
 #
 # Note that for the NT service to build correctly, you need at least
 # win32all build 161, for the COM samples, you need build 163.
+# Requires wxPython, and Tim Golden's WMI module.
 
 from distutils.core import setup
 import py2exe
@@ -12,11 +13,14 @@ if len(sys.argv) == 1:
     sys.argv.append("py2exe")
     sys.argv.append("-q")
 
-################################################################
-# A program using early bound COM, needs the typelibs option below
-test_wmi = dict(
-    script = "test_wmi.py",
-    )
+class Target:
+    def __init__(self, **kw):
+        self.__dict__.update(kw)
+        # for the versioninfo resources
+        self.version = "0.5.0"
+        self.company_name = "No Company"
+        self.copyright = "no copyright"
+        self.name = "py2exe sample files"
 
 ################################################################
 # A program using wxPython
@@ -24,9 +28,9 @@ test_wmi = dict(
 # The manifest will be inserted as resource into test_wx.exe.  This
 # gives the controls the Windows XP appearance (if run on XP ;-)
 #
-# Another option would be to store if in a file named
-# test_wx.exe.manifest, and probably copy it with the data_files
-# option.
+# Another option would be to store it in a file named
+# test_wx.exe.manifest, and copy it with the data_files option into
+# the dist-dir.
 #
 manifest_template = '''
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
@@ -55,19 +59,39 @@ manifest_template = '''
 
 RT_MANIFEST = 24
 
-test_wx = dict(
+test_wx = Target(
+    # used for the versioninfo resource
+    description = "A sample GUI app",
+
+    # what to build
     script = "test_wx.py",
     other_resources = [(RT_MANIFEST, 1, manifest_template % dict(prog="test_wx"))],
+    icon_resources = [(1, "icon.ico")],
     dest_base = "test_wx")
 
-test_wx_console = dict(
+test_wx_console = Target(
+    # used for the versioninfo resource
+    description = "A sample GUI app with console",
+
+    # what to build
     script = "test_wx.py",
     other_resources = [(RT_MANIFEST, 1, manifest_template % dict(prog="test_wx"))],
     dest_base = "test_wx_console")
 
 ################################################################
+# A program using early bound COM, needs the typelibs option below
+test_wmi = Target(
+    description = "Early bound COM client example",
+    script = "test_wmi.py",
+    )
+
+################################################################
 # a NT service, modules is required
-myservice = dict(
+myservice = Target(
+    # used for the versioninfo resource
+    description = "A sample Windows NT service",
+    # what to build.  For a service, the module name (not the
+    # filename) must be specified!
     modules = ["MyService"]
     )
 
@@ -77,11 +101,15 @@ myservice = dict(
 # If you only want a dll or an exe, comment out one of the create_xxx
 # lines below.
 
-interp = dict(
+interp = Target(
+    description = "Python Interpreter as COM server module",
+    # what to build.  For COM servers, the module name (not the
+    # filename) must be specified!
     modules = ["win32com.servers.interp"],
 ##    create_exe = False,
 ##    create_dll = False,
     )
+
 ################################################################
 # COM pulls in a lot of stuff which we don't want or need.
 
@@ -90,17 +118,18 @@ excludes = ["pywin", "pywin.debugger", "pywin.debugger.dbgcon",
 
 setup(
     options = {"py2exe": {"typelibs":
+                          # typelib for WMI
                           [('{565783C6-CB41-11D1-8B02-00600806D9B6}', 0, 1, 2)],
                           # create a compressed zip archive
                           "compressed": 1,
                           "optimize": 2,
                           "excludes": excludes}},
     # The lib directory contains everything except the executables and the python dll.
+    # Can include a subdirectory name.
     zipfile = "lib/shared.zip",
+
     service = [myservice],
-    # a COM server, build as exe *and* dll
-    com_server = ["win32com.servers.interp"],
-    # other stuff
+    com_server = [interp],
     console = [test_wx_console, test_wmi],
     windows = [test_wx],
     )
