2de15bf3b31df6ef15da6a884d4e0c5faa0bb771
theller <theller@b94a6edd-cd4f-0410-beef-d2780d74e76e>
2004-01-16 19:24:56 +0000

    *** empty log message ***

---
+++ trunk/py2exe/docs/updateweb.cmd
@@ -0,0 +1,2 @@
+scp index.html default.css LICENSE.txt py2exe.jpg theller@starship.python.net:~/public_html/py2exe
+scp index.html default.css LICENSE.txt py2exe.jpg theller@shell.sourceforge.net:/home/groups/p/py/py2exe/htdocs
