ca851ba567193de42d1816311a26607a553b592f
theller <theller@b94a6edd-cd4f-0410-beef-d2780d74e76e>
2004-02-02 08:50:30 +0000

    Bump version number.

--- trunk/py2exe/py2exe/__init__.py
+++ trunk/py2exe/py2exe/__init__.py
@@ -59,7 +59,7 @@ keys in the dictionary are recognized, most are optional:
 # special one contained in this module.
 #
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 import distutils.dist, distutils.core, distutils.command, build_exe, sys
 
