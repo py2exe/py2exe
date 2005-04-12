2dbb54e141d18e8fa3f140483d29f032e5cab5d3
Thomas Heller <theller@ctypes.org>
2005-04-12 07:43:46 +0000

    Bump version number.

--- py2exe/py2exe/__init__.py
+++ py2exe/py2exe/__init__.py
@@ -62,7 +62,7 @@ keys in the dictionary are recognized, most are optional:
 # special one contained in this module.
 #
 
-__version__ = "0.5.5"
+__version__ = "0.6.0"
 
 import distutils.dist, distutils.core, distutils.command, build_exe, sys
 
