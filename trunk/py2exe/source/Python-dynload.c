9656d575d8af74ed7908906bd150be0792633b75
theller <theller@b94a6edd-cd4f-0410-beef-d2780d74e76e>
2005-04-21 19:45:58 +0000

    For runtime linking to the Python dll, "Python-dynload.c" must be included instead of <Python.h>, the exe must be linked with "Python-dynload.c", and _load_python() must be called before using any Python functions.

---
+++ trunk/py2exe/source/Python-dynload.c
@@ -0,0 +1,65 @@
+/* **************** Python-dynload.c **************** */
+#include "Python-dynload.h"
+#include "../hacks/memimp/MemoryModule.h"
+
+struct IMPORT imports[] = {
+#include "import-tab.c"
+	{ NULL, NULL }, /* sentinel */
+};
+
+void Py_XDECREF(PyObject *ob)
+{
+	static PyObject *tup;
+	if (tup == NULL)
+		tup = PyTuple_New(1);
+	/* Let the tuple take the refcount */
+	PyTuple_SetItem(tup, 0, ob);
+	/* and overwrite it */
+	PyTuple_SetItem(tup, 0, PyInt_FromLong(0));
+}
+
+int _load_python_FromFile(char *dllname)
+{
+	int i;
+	struct IMPORT *p = imports;
+	HMODULE hmod;
+	
+	hmod = LoadLibrary(dllname);
+	if (hmod == NULL) {
+		return 0;
+	}
+	for (i = 0; p->name; ++i, ++p) {
+		p->proc = (void (*)())GetProcAddress(hmod, p->name);
+		if (p->proc == NULL) {
+			OutputDebugString("undef symbol");
+			printf("undefined symbol %s -> exit(-1)\n", p->name);
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int _load_python(char *dllname, char *bytes)
+{
+	int i;
+	struct IMPORT *p = imports;
+	HMODULE hmod;
+
+	if (!bytes)
+		return _load_python_FromFile(dllname);
+
+	hmod = MemoryLoadLibrary(dllname, bytes, NULL, NULL);
+	if (hmod == NULL) {
+		return 0;
+	}
+	for (i = 0; p->name; ++i, ++p) {
+		p->proc = (void (*)())MemoryGetProcAddress(hmod, p->name);
+		if (p->proc == NULL) {
+			OutputDebugString("undef symbol");
+			printf("undefined symbol %s -> exit(-1)\n", p->name);
+			return 0;
+		}
+	}
+	return 1;
+}
+
