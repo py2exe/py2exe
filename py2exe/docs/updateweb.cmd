e67f237177b621fc9d1778f649a634c0d169a3b5
Jimmy Retzlaff <453647+jretz@users.noreply.github.com>
2005-10-09 12:15:09 +0000

    Change from Thomas' site/credentials for publishing to Jimmy's.

--- py2exe/docs/updateweb.cmd
+++ py2exe/docs/updateweb.cmd
@@ -1,2 +1,2 @@
-scp index.html default.css LICENSE.txt py2exe.jpg theller@starship.python.net:~/public_html/py2exe
-scp index.html default.css LICENSE.txt py2exe.jpg theller@shell.sourceforge.net:/home/groups/p/py/py2exe/htdocs
+scp index.html default.css LICENSE.txt py2exe.jpg jretz@py2exe.org:~/public_html/py2exe
+scp index.html default.css LICENSE.txt py2exe.jpg jretz@shell.sourceforge.net:/home/groups/p/py/py2exe/htdocs
