32c07fe1b2dba66f10182af53cbb0aae0492bc38
theller <theller@b94a6edd-cd4f-0410-beef-d2780d74e76e>
2006-11-22 08:04:19 +0000

    Correct the typedef for Py_ssize_t on both win32 and win64.

--- trunk/py2exe/source/Python-version.h
+++ trunk/py2exe/source/Python-version.h
@@ -4,9 +4,12 @@
    typedef int Py_ssize_t;
 #else
 #  define PYTHON_API_VERSION 1013
-#  if defined (_WIN32)
-     typedef long Py_ssize_t;
-#  elif defined (_WIN64)
+/* The check for _WIN64 must come first, because on win64 both _WIN64 and
+ *  _WIN32 are defined!
+ */
+#  if defined (_WIN64)
      typedef __int64 Py_ssize_t;
+#  elif defined (_WIN32)
+     typedef int Py_ssize_t;
 #  endif
 #endif
