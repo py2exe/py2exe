6ab7d1a8f59827eba7234f7f63bbc3d8d9dd44db
Jimmy Retzlaff <453647+jretz@users.noreply.github.com>
2006-12-31 12:50:00 +0000

    Now points to py2exe.org.

--- py2exe/docs/updateweb.cmd
+++ py2exe/docs/updateweb.cmd
@@ -1,2 +1,2 @@
-scp index.html default.css LICENSE.txt py2exe.jpg jretz@py2exe.org:~/public_html/py2exe
+scp index.html default.css LICENSE.txt py2exe.jpg jretz@py2exe.org:~/py2exe.org/old
 scp index.html default.css LICENSE.txt py2exe.jpg jretz@shell.sourceforge.net:/home/groups/p/py/py2exe/htdocs
