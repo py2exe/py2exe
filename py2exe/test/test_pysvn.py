e2b39a497ec5700b20b1bee8b44789ef0c58c671
Jimmy Retzlaff <453647+jretz@users.noreply.github.com>
2008-05-19 07:13:41 +0000

    Without supplying a setup_pysvn.py we can't be sure we get the same version number output when running this test under Python and under py2exe (because different svn dlls are found). So just print the major version number (which still tests that we can retrieve the version number).

--- py2exe/test/test_pysvn.py
+++ py2exe/test/test_pysvn.py
@@ -1,4 +1,8 @@
 import pysvn
 
 if __name__ == "__main__":
-    print pysvn.version
+    # the py2exe wrapper for this test doesn't pick up the svn
+    # lib installed with pysvn so it can report a different
+    # version number, so just make sure we can get the version
+    # number and that the major version is the same
+    print pysvn.version[0]
