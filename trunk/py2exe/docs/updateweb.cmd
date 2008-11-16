fd8ea4b9cf805e5e2032a5874853ddc66bf83618
jretz <jretz@b94a6edd-cd4f-0410-beef-d2780d74e76e>
2008-11-16 07:23:46 +0000

    Removed SourceForge from the web site upload batch file as it is no longer used.

--- trunk/py2exe/docs/updateweb.cmd
+++ trunk/py2exe/docs/updateweb.cmd
@@ -1,2 +1,1 @@
 scp index.html default.css LICENSE.txt py2exe.jpg jretz@py2exe.org:~/py2exe.org/old
-scp index.html default.css LICENSE.txt py2exe.jpg jretz@shell.sourceforge.net:/home/groups/p/py/py2exe/htdocs
