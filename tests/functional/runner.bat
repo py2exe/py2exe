d02ab4c6124bd6285a4c25dbafda295606251032
Alberto Sottile <alby128@gmail.com>
2019-10-20 11:13:35 +0200

    test runner: support optional post_build script

--- tests/functional/runner.bat
+++ tests/functional/runner.bat
@@ -3,6 +3,7 @@ set testname=%1
 cd %testname%
 if exist .\requirements.txt pip install -r requirements.txt
 python setup.py py2exe
+if exist .\post_build.bat .\post_build.bat
 cd dist
 .\%testname%.exe
 cd ..
---
+++ tests/functional/ssl_test/post_build.bat
@@ -0,0 +1,4 @@
+  del C:\Windows\System32\libssl*.dll /F
+  del C:\Windows\SysWOW64\libssl*.dll /F
+  del C:\Windows\System32\libcrypto*.dll /F
+  del C:\Windows\SysWOW64\libcrypto*.dll /F
