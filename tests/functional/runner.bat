b11b4575210bb2ba9e9ffabb8b5fa483fd288cc1
albertosottile <alby128@gmail.com>
2020-10-05 14:53:38 +0200

    Fail AppVeyor build if functional tests tail

--- tests/functional/runner.bat
+++ tests/functional/runner.bat
@@ -1,12 +1,25 @@
+@echo off
 set testname=%1
+set TESTFAILED=0
 
 cd %testname%
 if exist .\requirements.txt pip install -r requirements.txt
 python setup.py py2exe
 if exist .\post_build.bat .\post_build.bat
 cd dist
-.\%testname%.exe
+%testname%.exe || set TESTFAILED=1
+goto :clean
+
+:clean
 cd ..
 rmdir dist /s /q
 if exist .\requirements.txt pip uninstall -r requirements.txt -y
 cd ..
+goto :exit
+
+:exit
+if %TESTFAILED% equ 1 (
+    echo %testname% FAILED!!!
+    exit /b -1
+)
+
