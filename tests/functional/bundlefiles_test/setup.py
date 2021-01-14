c1b3ae2bd5fae1de8378fe18d9fc56b1747057ff
Alberto Sottile <asottile@gmail.com>
2021-01-14 18:05:54 +0100

    Add bundlefiles test to AppVeyor

--- tests/functional/bundlefiles_test/setup.py
+++ tests/functional/bundlefiles_test/setup.py
@@ -1,24 +1,7 @@
 from distutils.core import setup
 import py2exe
 
-mainfile ='bundlefiles_test.py'
-exefile = 'bundlefiles_test'
-
-setup(name="Main",
-      version='0.0',
-      description="Main script",
-      scripts=[mainfile],
-      windows=[{"script": mainfile,
-                'company_name': "Company (c) 2019",
-                'copyright': "Company (c) 2019",
-                'dest_base': exefile}],
-      data_files = [],
-      options={"py2exe": {"unbuffered": True,
-                          "optimize": 2,
-                          "bundle_files": 0,
-                          "compressed": False,
-                          "includes": [],
-                          "excludes" : [],
-                          "packages": [],
-                          "dll_excludes": []},},
-      zipfile=None)
+setup(console=[{ "script": "bundlefiles_test.py"}],
+    options={"py2exe": {
+        "bundle_files": 2,
+        "verbose": 4}})
