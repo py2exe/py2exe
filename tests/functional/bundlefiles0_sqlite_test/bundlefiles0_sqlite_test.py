f258f06726d717b8f99a98e63bd2ce18fcaee3d4
Alberto Sottile <asottile@gmail.com>
2021-01-17 18:59:41 +0100

    Add tests for bundle_files=0

---
+++ tests/functional/bundlefiles0_sqlite_test/bundlefiles0_sqlite_test.py
@@ -0,0 +1,20 @@
+import os
+import sqlite3
+
+print('sqlite3 version: {}'.format(sqlite3.version))
+
+conn = sqlite3.connect('test.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE stocks
+             (date text, trans text, symbol text, qty real, price real)''')
+c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+conn.commit()
+
+for row in c.execute('SELECT * FROM stocks ORDER BY price'):
+        out = row
+
+print('sqlite3 test output: {}'.format(out))
+assert str(out) == "('2006-01-05', 'BUY', 'RHAT', 100.0, 35.14)"
+
+conn.close()
+os.remove('test.db')
---
+++ tests/functional/bundlefiles0_ssl_test/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+import py2exe
+
+setup(console=[{ "script": "bundlefiles0_ssl_test.py"}],
+    options={"py2exe": {
+        "bundle_files": 0,
+        "excludes": "tkinter",
+        "verbose": 4}},
+     #zipfile=None
+     )
