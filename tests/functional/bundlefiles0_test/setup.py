b7509d30ad4c9419f524e25282c69f804b9bf55c
Alberto Sottile <asottile@gmail.com>
2021-01-17 23:32:28 +0100

    Exclude _ssl from bundle_files=0 test

--- tests/functional/bundlefiles0_test/setup.py
+++ tests/functional/bundlefiles0_test/setup.py
@@ -4,6 +4,6 @@ import py2exe
 setup(console=[{ "script": "bundlefiles0_test.py"}],
     options={"py2exe": {
         "bundle_files": 0,
-        "excludes": "tkinter",
+        "excludes": "tkinter, _ssl",
         "verbose": 4}},
      zipfile=None)
