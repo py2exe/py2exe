4728bac9fd21a638120d52ee6dedabd6e82a5f10
Alberto Sottile <asottile@gmail.com>
2022-09-04 23:27:57 +0200

    Temporarily disable wxPython test

--- tests/functional/enable_tests.py
+++ tests/functional/enable_tests.py
@@ -10,6 +10,13 @@ tests = list(map(os.path.basename, tests))
 if sys.version_info >= (3, 10):
     tests = [t for t in tests if t[0] != '_']
 
+# temporarily disable _wxPython test
+# https://github.com/wxWidgets/Phoenix/issues/2246
+try:
+    tests.remove('_wxPython_test')
+except ValueError:
+    pass
+
 print(f'Enabled tests: {tests}')
 
 with open('enabled_tests.txt', 'w') as f:
