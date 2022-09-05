ece8c78b412cff9a88abbe054ac2f4e794ca39f9
Alberto Sottile <asottile@gmail.com>
2022-09-05 19:29:01 +0200

    nvda_test: fixup

--- tests/functional/nvda_test/freeze.py
+++ tests/functional/nvda_test/freeze.py
@@ -1,6 +1,6 @@
-from distutils.core import setup
-import py2exe
-setup(
+from py2exe import freeze
+
+freeze(
 	console=[{"script": "nvda_test.py",},],
 	data_files=[('.', ['data.txt'])],
     options={"py2exe": {
